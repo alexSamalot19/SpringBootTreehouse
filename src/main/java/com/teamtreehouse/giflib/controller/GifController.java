f38ac875f48451adfddeefc81b2814cb1260b27a
alexSam <alexsamalot19@gmail.com>
2020-03-18 11:05:46 -0400

    Controller with request mapping

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,14 +1,26 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
     @RequestMapping("/")
     public String listGifs(){
         return "home";
+    }
+
+    // Request mapping to Gif URI
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("Compiler-bot", LocalDate.of(2015, 2, 13), "Chris Rama", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 
 }
